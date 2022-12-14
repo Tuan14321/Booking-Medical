cd45462b37b39ae6d9e6b5424a9ce033b4165e47
tuan <tuann14321@gmail.com>
2022-12-14 21:34:35 +0700

    demo-connect-database

--- src/route/web.js
+++ src/route/web.js
@@ -1,16 +1,15 @@
 import express from 'express';
+import homeController from '../controllers/HomeController';
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
 
-    router.get('/', (req, res) => {
-        return res.send("Hello world")
-    });
+    router.get('/', homeController.getHomePage);
 
-    // router.get('/tuan', (req, res) => {
-    //     return res.send("Hello Tuan");
-    // });
+    router.get('/tuan', (req, res) => {
+        return res.send("Hello Tuan");
+    });
     return app.use('/', router);
 }
 
