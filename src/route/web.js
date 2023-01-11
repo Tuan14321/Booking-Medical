bc99ca056fe075861da0d8ad86fc35a4456ad62f
tuan <tuann14321@gmail.com>
2023-01-11 16:09:35 +0700

    CRUD

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,10 @@ let router = express.Router();
 let initWebRoutes = (app) => {
 
     router.get('/', homeController.getHomePage);
+    router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
+
+
 
     router.get('/tuan', (req, res) => {
         return res.send("Hello Tuan");
